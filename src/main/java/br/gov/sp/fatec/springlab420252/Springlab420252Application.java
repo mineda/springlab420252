fb003e13c47613bd4c9ce4d0a3550d5ddc4c7b8a
Emanuel Mineda Carneiro <mineda@gmail.com>
2025-08-14 20:04:43 -0300

    first commit

---
+++ src/main/java/br/gov/sp/fatec/springlab420252/Springlab420252Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springlab420252;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springlab420252Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springlab420252Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/springlab420252/Springlab420252ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springlab420252;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springlab420252ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
