a98d86f5a3841e4d070999bb83f978afff5b9ef6
Emanuel Mineda Carneiro <mineda@gmail.com>
2025-09-09 23:49:32 +0000

    Retirando imports desnecessários

--- src/main/java/br/gov/sp/fatec/springlab420252/controller/TrabalhoController.java
+++ src/main/java/br/gov/sp/fatec/springlab420252/controller/TrabalhoController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.gov.sp.fatec.springlab420252.entity.Trabalho;
-import br.gov.sp.fatec.springlab420252.entity.Usuario;
 import br.gov.sp.fatec.springlab420252.service.TrabalhoService;
 
 @RestController
